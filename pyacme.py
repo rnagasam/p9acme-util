cd33a8d5e167d7eec5695cf6183c9e86f19aaee2
Ramana Nagasamudram <rnagasam@stevens.edu>
2018-01-07 23:35:01 -0500

    generic write and read functions

--- pyacme.py
+++ pyacme.py
@@ -1,68 +1,30 @@
 import subprocess
 
-# TODO
-# Many more wrappers around `9p`
-# Abstract subprocess calls into a function to avoid repeating code like below
+def write(s, id, loc):
+	"""
+	Write string s, into window id's loc file
+	
+	runs
+		echo -n s | 9p write acme/id/loc
+	"""
+	out = subprocess.Popen(['echo', '-n', s], stdout=subprocess.PIPE)
+	wrt = subprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/'+loc], stdin=out.stdout)
+	return
+	
+def read(id, loc):
+	"""
+	Read from window id's loc file
+	
+	runs
+		9p read acme/id/loc
+	returns
+		utf-8 encoded string with contents of acme/id/loc
+	"""
+	s = subprocess.check_output(['9p', 'read', 'acme/'+str(id)+'/'+loc])
+	return s.decode('utf-8')
 
 def windownew():
 	"""Create a new ACME window and return
 	the window ID"""
 	winid = subprocess.check_output(['9p', 'read', 'acme/new/ctl']).split()[0]
 	return int(winid)
-	
-def writebody(id, s):
-	"""Write to the body of a window"""
-	echos = subprocess.Popen(['echo', '-n', s], stdout=subprocess.PIPE)
-	subprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/body'], stdin=echos.stdout)
-	return
-
-def writectl(id, s):
-	"""Write to the ctl of a window"""
-	echos = subprocess.Popen(['echo', '-n', s], stdout=subprocess.PIPE)
-	p = subprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/ctl'], stdin=echos.stdout)
-	return
-	
-def writedata(id, s):
-	"""Write to the data of a window"""
-	echos = subprocess.Popen(['echo', '-n', s], stdout=subprocess.PIPE)
-	p = subprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/data'], stdin=echos.stdout)
-	return
-	
-def writename(id, s):
-	"""Set the name of a window"""
-	echos = subprocess.Popen(['echo', '-n', s], stdout=subprocess.PIPE)
-	p = subprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/data'], stdin=echos.stdout)
-	return
-	
-def writeaddr(id, s):
-	"""Write to the addr of a window"""
-	echos = subprocess.Popen(['echo', '-n', s], stdout=subprocess.PIPE)
-	p = subprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/addr'], stdin=echos.stdout)
-	return
-	
-def readbody(id):
-	"""Read from the body of a window"""
-	s = subprocess.check_output(['9p', 'read', 'acme/'+str(id)+'/body'])
-	return s.decode('utf-8')
-
-def readctl(id):
-	"""Read from the ctl of a window"""
-	s = subprocess.check_output(['9p', 'read', 'acme/'+str(id)+'/ctl'])
-	return s.decode('utf-8')
-
-def readaddr(id):
-	"""Read from the addr of a window"""
-	s = subprocess.check_output(['9p', 'read', 'acme/'+str(id)+'/addr'])
-	return s.decode('utf-8')
-
-def readdata(id):
-	"""Read from the data of a window"""
-	s = subprocess.check_output(['9p', 'read', 'acme/'+str(id)+'/addr'])
-	return s.decode('utf-8')
-
-def syncpos(id):
-	"""Set dot=addr"""
-	s = subprocess.Popen(['echo', '-n', 'dot=addr'], stdout=subprocess.PIPE)
-	p = suboprocess.Popen(['9p', 'write', 'acme/'+str(id)+'/body'])
-	return
-	
